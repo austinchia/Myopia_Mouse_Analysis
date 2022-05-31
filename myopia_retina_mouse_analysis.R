b04738edf63b988097e5597ae92b96e82f9f5263
austinchia <austin.chia.ce@gmail.com>
2022-05-31 23:17:20 +0800

    made changes

--- myopia_retina_mouse_analysis.R
+++ myopia_retina_mouse_analysis.R
@@ -1,14 +1,14 @@
-library("readxl")
-
-# Set 1
-abund_s1 <- data.frame(read_excel("Myopia_retina_Whole Proteome results_3 sets.xlsx",sheet="Retina_WP_S1_abundance_ratios"))                                                                            
-abund_log_s1 <- data.frame(read_excel("Myopia_retina_Whole Proteome results_3 sets.xlsx",sheet="Retina_WP_S1_abundance_log2"))                                                                            
-abund_group_s1 <- data.frame(read_excel("Myopia_retina_Whole Proteome results_3 sets.xlsx",sheet="Retina_WP_S1_abundance_grouped"))                                                                            
-
-# Set 2
-abund_s2 <- data.frame(read_excel("Myopia_retina_Whole Proteome results_3 sets.xlsx",sheet="Retina_WP_S2_abundance_ratios"))                                                                            
-abund_log_s2 <- data.frame(read_excel("Myopia_retina_Whole Proteome results_3 sets.xlsx",sheet="Retina_WP_S2_abundance_log2"))                                                                            
-abund_group_s2 <- data.frame(read_excel("Myopia_retina_Whole Proteome results_3 sets.xlsx",sheet="Retina_WP_S2_abundance_grouped"))                                                                            
+# library("readxl")
+# 
+# # Set 1
+# abund_s1 <- data.frame(read_excel("Myopia_retina_Whole Proteome results_3 sets.xlsx",sheet="Retina_WP_S1_abundance_ratios"))                                                                            
+# abund_log_s1 <- data.frame(read_excel("Myopia_retina_Whole Proteome results_3 sets.xlsx",sheet="Retina_WP_S1_abundance_log2"))                                                                            
+# abund_group_s1 <- data.frame(read_excel("Myopia_retina_Whole Proteome results_3 sets.xlsx",sheet="Retina_WP_S1_abundance_grouped"))                                                                            
+# 
+# # Set 2
+# abund_s2 <- data.frame(read_excel("Myopia_retina_Whole Proteome results_3 sets.xlsx",sheet="Retina_WP_S2_abundance_ratios"))                                                                            
+# abund_log_s2 <- data.frame(read_excel("Myopia_retina_Whole Proteome results_3 sets.xlsx",sheet="Retina_WP_S2_abundance_log2"))                                                                            
+# abund_group_s2 <- data.frame(read_excel("Myopia_retina_Whole Proteome results_3 sets.xlsx",sheet="Retina_WP_S2_abundance_grouped"))                                                                            
 
 ############# Metaboanalyst ###############
 
@@ -38,12 +38,19 @@ mSet <- PlotVolcano(mSet, "volcano_abundance_S1",1, 0, "png", 72, width=NA)
 mSet <- SaveTransformedData(mSet)
 
 ############## 
-# test <- data.frame(read.csv("abundance_S1_test.csv"))
+library(data.table)
+test <- fread("data_normalized.csv")
+
+group1median <- as.numeric(apply(test[2:8], 1, FUN=median))
+group2median <- as.numeric(apply(test[9:15], 1, FUN=median))
+FC <- (group1median/group2median)
+log2FC <- log(FC,2)
+
 # 
-# group1mean <- apply(test[2:8], 1, FUN=mean)
-# group2mean <- apply(test[9:16], 1, FUN=mean)
-# FC <- group1mean/group2mean 
-# log2FC <- log(FC,2)
-# 
-# pvalue <- apply(test,1, function(x)
-#   {t.test(x[2:8], x[9:16])$p.value} )
+pvalue <- apply(test,1, function(x) {t.test(x[2:8], x[9:16])$p.value} )
+
+
+# hr_0 <- c(t.test(test$X.LI.0hr...GC.1., test$X.NL.0hr...GC.1.)$p.value)
+
+
+
