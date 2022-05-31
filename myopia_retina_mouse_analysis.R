5505e0400101230b0151590a8e33a80ab2374abb
austinchia <austin.chia.ce@gmail.com>
2022-05-31 18:46:14 +0800

    made changes

--- myopia_retina_mouse_analysis.R
+++ myopia_retina_mouse_analysis.R
@@ -10,27 +10,40 @@ abund_s2 <- data.frame(read_excel("Myopia_retina_Whole Proteome results_3 sets.x
 abund_log_s2 <- data.frame(read_excel("Myopia_retina_Whole Proteome results_3 sets.xlsx",sheet="Retina_WP_S2_abundance_log2"))                                                                            
 abund_group_s2 <- data.frame(read_excel("Myopia_retina_Whole Proteome results_3 sets.xlsx",sheet="Retina_WP_S2_abundance_grouped"))                                                                            
 
+############# Metaboanalyst ###############
+
 library("MetaboAnalystR")
 # initializing object
-mSet<-InitDataObjects("conc", "stat", FALSE)
+mSet <- InitDataObjects("pktable", "stat", FALSE)
 # loading in data
-mSet<-Read.TextData(mSet, "abundance_S1_test.csv", "colu", "disc");
+mSet <- Read.TextData(mSet, "abundance_S1_test.csv", "colu", "disc");
 # data check
-mSet<-SanityCheckData(mSet)
-mSet<-ReplaceMin(mSet);
+mSet <- SanityCheckData(mSet)
+mSet <- ReplaceMin(mSet);
 
 # filtering features
-mSet<-FilterVariable(mSet, "none", "F", 25)
+mSet <- FilterVariable(mSet, "none", "F", 25)
 
 # median normalization, log10 transformation, pareto scaling
-mSet<-PreparePrenormData(mSet)
-mSet<-Normalization(mSet, "MedianNorm", "LogNorm", "ParetoNorm", ratio=FALSE, ratioNum=20)
-mSet<-PlotNormSummary(mSet, "norm_0_", "png", 72, width=NA)
-mSet<-PlotSampleNormSummary(mSet, "snorm_0_", "png", 72, width=NA)
+mSet <- PreparePrenormData(mSet)
+mSet <- Normalization(mSet, "MedianNorm", "LogNorm", "ParetoNorm", ratio=FALSE, ratioNum=20)
+mSet <- PlotNormSummary(mSet, "norm_0_", "png", 72, width=NA)
+mSet <- PlotSampleNormSummary(mSet, "snorm_0_", "png", 72, width=NA)
 
 # plot volcano plot
-mSet<-Volcano.Anal(mSet, FALSE, 1.5, 0, F, 0.05, TRUE, "raw")
-mSet <-PlotVolcano(mSet, "volcano_1_",1, 0, "png", 72, width=NA)
+mSet <- Volcano.Anal(mSet, FALSE, 1.5, 0, F, 0.05, TRUE, "raw")
+mSet <- PlotVolcano(mSet, "volcano_abundance_S1",1, 0, "png", 72, width=NA)
 
 # save transformed dataset
-mSet<-SaveTransformedData(mSet)
+mSet <- SaveTransformedData(mSet)
+
+############## 
+# test <- data.frame(read.csv("abundance_S1_test.csv"))
+# 
+# group1mean <- apply(test[2:8], 1, FUN=mean)
+# group2mean <- apply(test[9:16], 1, FUN=mean)
+# FC <- group1mean/group2mean 
+# log2FC <- log(FC,2)
+# 
+# pvalue <- apply(test,1, function(x)
+#   {t.test(x[2:8], x[9:16])$p.value} )
