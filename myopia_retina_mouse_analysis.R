1ccf97032bbcafb724049167cffec397a178d0fe
austinchia <austin.chia.ce@gmail.com>
2022-05-31 10:19:36 +0800

    added loaded dataset

---
+++ myopia_retina_mouse_analysis.R
@@ -0,0 +1,6 @@
+library("readxl")
+
+mouse <- read_excel("Myopia_retina_Whole Proteome results_3 sets.xlsx",sheet="Retina_WP_S1_abundance_ratios")                                                                            
+abund_s1 <- data.frame(mouse)
+
+Retina_WP_S2_abundance_ratios
